b54ea0ceece66b38d0f65fa7911baa923fe9866c
spidey0023 <nareshmohan0023@gmail.com>
2024-10-03 22:56:08 +0530

    Added sticky header and bluring other elements while on focus

--- DOM-Bankist/script.js
+++ DOM-Bankist/script.js
@@ -10,6 +10,10 @@ const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 const btnScrollTo = document.querySelector('.btn--scroll-to');
 const section1 = document.querySelector('#section--1');
 const nav__links = document.querySelector('.nav__links');
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+const nav = document.querySelector('.nav');
 
 const openModal = function () {
   modal.classList.remove('hidden');
@@ -66,9 +70,6 @@ nav__links.addEventListener('click', function (e) {
 });
 
 //Tabbed component
-const tabs = document.querySelectorAll('.operation__tab');
-const tabsContainer = document.querySelector('.operations__tab-container');
-const tabsContent = document.querySelectorAll('.operations__content');
 
 tabsContainer.addEventListener('click', function (e) {
   const clicked = e.target.closest('.operations__tab');
@@ -88,4 +89,31 @@ tabsContainer.addEventListener('click', function (e) {
   document
     .querySelector(`.operations__content--${clicked.dataset.tab}`)
     .classList.add('operations__content--active');
+});
+
+//Menu fade animation
+
+const handHover = function (e) {
+  if (e.target.classList.contains('nav__link')) {
+    const link = e.target;
+    const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+    const logo = link.closest('.nav').querySelector('img');
+
+    siblings.forEach(el => {
+      if (el !== link) el.style.opacity = this;
+    });
+    logo.style.opacity = this;
+  }
+};
+nav.addEventListener('mouseover', handHover.bind(0.5));
+nav.addEventListener('mouseout', handHover.bind(1));
+
+//Sticky Navigation
+const intailcoords = section1.getBoundingClientRect();
+console.log(intailcoords);
+
+window.addEventListener('scroll', function (e) {
+  console.log(e);
+  if (this.window.scrollY > intailcoords.top) nav.classList.add('sticky');
+  else nav.classList.remove('sticky');
 });
